de8a648a901d5b5463f22362e5fbac50d5d487f6
Nikhithareddie <149290975+Nikhithareddie@users.noreply.github.com>
2023-12-11 21:18:11 +0530

    Update connect.php

--- account/includes/connect.php
+++ account/includes/connect.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$servername = "localhost";
-$server_user = "root";
-$server_pass = "";
-$dbname = "food";
+$servername = "yummyfood.mysql.database.azure.com";
+$server_user = "yummyfood";
+$server_pass = "Nikkicloud@123";
+$dbname = "yummyfood";
 $name = $_SESSION['name'];
 $role = $_SESSION['role'];
 $con = new mysqli($servername, $server_user, $server_pass, $dbname);
